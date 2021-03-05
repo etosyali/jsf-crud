f35c8abfa501f4f5001cc395554976cb61d87bf1
etosyali <80028812+etosyali@users.noreply.github.com>
2021-03-05 09:25:01 +0300

    Add files via upload

---
+++ jsf-crud/src/main/java/org/ismek/beans/RehberManagedBean.java
@@ -0,0 +1,106 @@
+package org.ismek.beans;
+
+import org.ismek.db.DbOperations;
+import org.ismek.domain.Rehber;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.Facelet;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean(name = "rehberMB")
+public class RehberManagedBean {
+
+    private int id;
+    private String isim;
+    private String telefon;
+    private List<Rehber> rehberList = new ArrayList<>();
+
+    public RehberManagedBean() {
+        HttpServletRequest req = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String rehberId = req.getParameter("rehberId");
+        if (rehberId != null) {
+            Rehber rehber = DbOperations.kisiGetir(Integer.valueOf(rehberId));
+            id = rehber.getId();
+            isim = rehber.getIsim();
+            telefon = rehber.getTelefon();
+        }
+    }
+
+    public void save() {
+        DbOperations.rehbereEkle(isim, telefon);
+        sendRedirect("/faces/index.xhtml");
+    }
+
+    public void edit(int id) {
+        sendRedirect("/faces/edit.xhtml?rehberId=" + id);
+    }
+
+    public void delete(int id) {
+        sendRedirect("/faces/delete.xhtml?rehberId=" + id);
+    }
+
+    private void sendRedirect(String url) {
+        FacesContext currentInstance = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = currentInstance.getExternalContext();
+        try {
+            externalContext.redirect(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        DbOperations.kisiGuncelle(id, isim, telefon);
+        sendRedirect("/faces/index.xhtml");
+    }
+
+    public void delete() {
+        DbOperations.kisiSil(id);
+        sendRedirect("/faces/index.xhtml");
+    }
+
+    public void ara() {
+        List<Rehber> rehberListesi = (List<Rehber>) DbOperations.kisiGetir(isim);
+        for (Rehber rehber : rehberListesi) {
+            System.out.println(rehber);
+        }
+    }
+
+    public List<Rehber> getRehberList() {
+        rehberList = (List<Rehber>) DbOperations.tumRehberiGetir();
+        return rehberList;
+    }
+
+    public void setRehberList(List<Rehber> rehberList) {
+        this.rehberList = rehberList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+}
